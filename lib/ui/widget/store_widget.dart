6e4ef82bb814eada1b9eeff03054a802b23a8eb9
bernademir <berna.demir90@gmail.com>
2021-03-31 13:16:36 +0300

    initial commit

---
+++ lib/ui/widget/store_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class StoreWidget extends StatelessWidget {
+  final String title;
+
+  const StoreWidget({Key key, this.title}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
